9a1bb9b01592e04bc22ca455efc5b5b11426c52f
Oleksii <nebezotveta@gmail.com>
2022-05-19 12:12:46 +0300

    Search panel added, lesson 41

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,29 @@
-function Main() {
- return <main className="container content">
-     Hello from React Movies App
- </main>
+import React, { Component } from 'react';
+import { Movies } from '../components/Movies';
+import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
+
+class Main extends Component {
+    state = {
+        movies: []
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=cd2c932d&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({ movies: data.Search }))
+    }
+
+    render() {
+        const { movies } = this.state;
+
+        return <main className="container content">
+            <Search/>
+            {movies.length ? <Movies movies={this.state.movies} /> : <Preloader/>}
+
+        </main>
+    }
+
 }
 
-export {Main}
+export { Main }
